4fb4c9ab5737d768bbea8e9f5702c12bf740ce1e
Jonas <jonas@skayo.dev>
2022-04-02 00:26:16 +0200

    Made entrypoint.sh executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,13 @@ set -e
 if [ "$#" -gt 0 ]; then
 	/usr/local/bin/trash "$@"
 else
-	echo "Creating crontab file..."
 	schedule=${CRON_SCHEDULE:-@daily}
 	sonarr_schedule=${CRON_SCHEDULE_SONARR:-$schedule}
 	radarr_schedule=${CRON_SCHEDULE_RADARR:-$schedule}
+
 	config_file=${CONFIG_FILE:-/config/trash.yml}
+
+	echo "Creating crontab file..."
 	echo "$sonarr_schedule /usr/local/bin/trash sonarr --config $config_file" > /var/spool/cron/crontabs/root
 	echo "$radarr_schedule /usr/local/bin/trash radarr --config $config_file" >> /var/spool/cron/crontabs/root
 
